fb770eaf2214f9733a3174363c21600dddc5a944
ghassan <ghassanalassadi@outlook.com>
2022-10-17 21:28:33 +1100

    started work on homepage

---
+++ client/src/pages/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <div className='who-we-are'>
+                <h1>Who We Are</h1>
+                <img src={require('../assets/guinness-beer.png')}></img>
+                <p>
+                    Guinness of Sydney serves to review beers in pubs all over Sydney. Specifically, Guinness beers.
+
+                </p>
+            </div>
+            <div className='the-perfect-pour'>
+                <h1>The Perfect Pour</h1>
+                {/* <img></img> */}
+                <p className='step-one'>
+                    What does it take to create the perfect pour?
+                    Well, first you must prepare a glass. A nice, tall Guinness glass is best.
+                </p>
+                <p className='step-two'>
+                    Tilt the glass. A good Guinness starts at an angle of 45 degrees.
+                </p>
+                <p className='step-three'>
+                    Pour. That's it, just pour. Pour until you reach the top of the harp logo.
+                </p>
+                <p className='step-four'>
+                    Now let it settle. Resist your temptation to drink. But only for a ninety-two and a half seconds.
+                </p>
+                <p className='step-five'>
+                    The glass isn't full yet. Time to top up.
+                </p>
+                <p className='step-six'>
+                    Perfection.
+                </p>
+            </div>
+            <div>
+                <img src={require('../assets/flann-pic.jpg')}></img>
+                <p className='quote'>
+                    "In time of trouble and lousey strife,
+                    You have still got a darlint plan
+                    You still can turn to a brighter life –
+                    A pint of plain is your only man."
+                    ~ Flann O'Brien
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
