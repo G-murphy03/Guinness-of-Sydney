c776f71a071cb5e4e8b08bd4e93d278adb293810
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-18 21:18:22 +1100

    basic review text form created

---
+++ client/src/components/ReviewInputs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ReviewInputs = () => {
+  return (
+    <div className='reviewInput'>
+      <h2> Add a Review</h2>
+      <form>
+        <label>Pub</label>
+        <input type='text' required />
+
+        <label>Review:</label>
+        <textarea required></textarea>
+
+        <label>Score:</label>
+        <input type='text' required />
+
+        <label>Price:</label>
+        <input type='text' required />
+
+        <label>Location:</label>
+        <input type='text' required />
+
+        <label>Reviewer:</label>
+        <input type='text' required />
+        <button>Add button</button>
+      </form>
+    </div>
+  );
+};
+
+export default ReviewInputs;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
-
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
@@ -19,7 +16,7 @@ const Profile = () => {
   const user = data?.me || data?.user || {};
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/me" />;
+    return <Navigate to='/me' />;
   }
 
   if (loading) {
@@ -37,8 +34,8 @@ const Profile = () => {
 
   return (
     <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+      <div className='flex-row justify-center mb-3'>
+        <h2 className='col-12 col-md-10 bg-dark text-light p-3 mb-5'>
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
