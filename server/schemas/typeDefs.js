23e9e9655b697eb39101da342412088713f7b54a
George Murphy <george.moguls@gmail.com>
2022-10-20 19:57:31 +1100

    server running, issue fixed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
 
     addReview(pubName: String!, review: String!, score: Int!, price: Float!, location: String!): Review
-    updateReview(pubName: String!, review: String!, score: Int!, price: Float!, location: String!): Review
+    updateReview(reviewId: ID!, pubName: String!, review: String!, score: Int!, price: Float!, location: String!): Review
     removeReview(reviewId: ID!): Review
   }
 `;
