aa2f3e04efd0e1995c4842d69cf586f2a6bbcc6a
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-10-24 19:17:01 +1100

    Basic styling fix for review cards, some seed data deleted, background css made static

--- client/src/components/ReviewList.js
+++ client/src/components/ReviewList.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-const Reviews = ({
-    reviews,
-}) => {
-    if (!reviews.length) {
-        return <h3>No reviews yet!</h3>;
-    }
-    return (
-        <div>
-            <h2>Viewing all reviews...</h2>
-            {reviews && reviews.map((review) => (
-                <Card style={{width: '18rem'}}>
-                    <Card.Img src />
-                    <Card.Body>
-                        <Card.Title className='pub-name'>{review.pubName}</Card.Title>
-                        <Card.Title className='pub-reviewer'>{review.reviewer}</Card.Title>
-                        <Card.Text className='pub-review'>{review.review}</Card.Text>
-                        <Card.Text className='pub-score'>{review.score}</Card.Text>
-                        <Card.Text className='pub-price'>{review.price}</Card.Text>
-                        <Card.Text className='pub-location'>{review.location}</Card.Text>
-                    </Card.Body>
-                </Card>
-            ))
-            }
-        </div>
-    )
+const Reviews = ({ reviews }) => {
+  return (
+    <div>
+      <h2>Viewing all reviews...</h2>
+      {reviews &&
+        reviews.map((review) => (
+          <CardGroup key={review._id}>
+            <Card.Img src />
+            <Card.Body className='card mb-3'>
+              <Card.Title className='pub-name' bg='dark'>
+                Pub Name: {review.pubName}
+              </Card.Title>
+              <Card.Title className='pub-reviewer' bg='dark'>
+                Reviewer: {review.reviewer}
+              </Card.Title>
+              <Card.Text className='pub-review' bg='dark'>
+                Review: {review.review}
+              </Card.Text>
+              <Card.Text className='pub-score' bg='dark'>
+                Score: {review.score}
+              </Card.Text>
+              <Card.Text className='pub-price' bg='dark'>
+                Price: {review.price}
+              </Card.Text>
+              <Card.Text className='pub-location' bg='dark'>
+                Location: {review.location}
+              </Card.Text>
+            </Card.Body>
+          </CardGroup>
+        ))}
+    </div>
+  );
 };
 
 export default Reviews;
